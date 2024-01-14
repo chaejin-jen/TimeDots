1cfbd7983b20623aaf936afecd970519af7d0a0f
chaejin-jen <jen71324@gmail.com>
2024-01-14 13:25:17 +0900

    chore: Set up ESLint import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,47 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['import'],
+  settings: {
+    'import/ignore': ['node_modules/react-native/index\\.js$'],
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '{react*,react*/**}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: './src/**/*',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always-and-inside-groups',
+        pathGroupsExcludedImportTypes: ['builtin', 'unknown'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
 };
